f1f0e588033907c52965f27cb1825841d237818d
Sharat Babu Jami <30460459+SharatBabu@users.noreply.github.com>
2019-05-28 15:43:25 +0530

    Update spiral_matrix.py

--- practice/spiral_matrix.py
+++ practice/spiral_matrix.py
@@ -1,5 +1,4 @@
-# Python3 program to print
-# given matrix in spiral form
+# Python3 program to print given matrix in spiral form
 
 
 def spiralMatrix(m, n, mat):
@@ -10,26 +9,23 @@ def spiralMatrix(m, n, mat):
         m - ending row index 
         l - starting column index 
         n - ending column index 
-        i - iterator '''
+    '''
 
     while k < m and l < n:
 
-        # Print the first row from
-        # the remaining rows
+        # Print the first row from the remaining rows
         for i in range(l, n):
             print(mat[k][i], end=" ")
 
         k += 1
 
-        # Print the last column from
-        # the remaining columns
+        # Print the last column from the remaining columns
         for i in range(k, m):
             print(mat[i][n - 1], end=" ")
 
         n -= 1
 
-        # Print the last row from
-        # the remaining rows
+        # Print the last row from the remaining rows
         if k < m:
 
             for i in range(n - 1, l - 1, -1):
@@ -37,8 +33,7 @@ def spiralMatrix(m, n, mat):
 
             m -= 1
 
-        # Print the first column from
-        # the remaining columns
+        # Print the first column from the remaining columns
         if l < n:
             for i in range(m - 1, k - 1, -1):
                 print(mat[i][l], end=" ")
@@ -46,7 +41,7 @@ def spiralMatrix(m, n, mat):
             l += 1
 
 
-# Driver Code
+# Matrix
 a = [[1, 2, 3, 4, 5, 6],
      [7, 8, 9, 10, 11, 12],
      [13, 14, 15, 16, 17, 18],
